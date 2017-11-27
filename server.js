2be2144b6cf37d617385b4139d4c97a74f7fa57b
EdMacall <irvedwmac@gmail.com>
2017-11-27 14:24:57 -0500

    fourth commit

--- server.js
+++ server.js
@@ -133,7 +133,16 @@ app.post("/articles/:id", function(req, res) {
       // If a Note was created successfully, find one Article with an `_id` equal to `req.params.id`. Update the Article to be associated with the new Note
       // { new: true } tells the query that we want it to return the updated User -- it returns the original by default
       // Since our mongoose query returns a promise, we can chain another `.then` which receives the result of the query
-      return db.Article.findOneAndUpdate({ _id: req.params.id }, { note: dbNote._id }, { new: true });
+      // return db.Article.findOneAndUpdate({ _id: req.params.id }, { note: dbNote._id }, { new: true });
+      db.Article.findOne({ _id: req.params.id })
+                .populate("note")
+                .then(function(dbArticle) {
+                      // If we were able to successfully find an Article with the given id,
+                      // send it back to the client
+                      var notes = dbArticle.note;
+                      notes.push(dbNote);
+                      return db.Article.findOneAndUpdate({ _id: req.params.id }, { note: notes }, { new: true });
+                });
     })
     .then(function(dbArticle) {
       // If we were able to successfully update an Article, send it back to the client
